8efed216f9e372c5b927553c1f0067c58e24d685
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-07 21:24:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,25 @@ class TicTacToe
       turn(board)
     end
   end
+  
+  def turn_count(board)
+    turn = 0 
+    board.each do |cell|
+      if cell == "X" || cell == "O"
+        turn += 1
+      end 
+    end
+    turn 
+  end 
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    "X"
+  else
+    "O"
+  end 
+end 
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
